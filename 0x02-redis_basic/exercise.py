ecaf236ae8da86f4dcdaac1386c03fd11292c48a
Tochukwu92 <ilonubatochukwu@gmail.com>
2024-10-23 20:17:27 +0000

    redis basic

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,7 +6,7 @@
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -29,3 +29,48 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Calable] = None) -> Optional[
+            Union[str, bytes, int, float]]:
+        '''
+        Retrive data from Redis, optionally applying a conversion function.
+
+        Args:
+            key (str): key to retrive data
+            fn (callable, optional): a function to apply to retrived data.
+
+        Return:
+            union[str, bytes, int float, None]:
+                The data converted using fn, or None if key doesn't exist.
+        '''
+        data = self._redis.get(key)
+        if data is None:
+            # Preserve the original Redis.get behavior when key doesn't exist
+            return None
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Optional[str]:
+        """
+        Retrieve data as a UTF-8 decoded string.
+
+        Args:
+            key (str): The key to retrieve.
+
+        Returns:
+            Optional[str]: The decoded string or None if the key doesn't exist.
+        """
+        return self.get(key, fn=lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """
+        Retrieve data as an integer.
+
+        Args:
+            key (str): The key to retrieve.
+
+        Returns:
+            Optional[int]: The integer value or None if the key doesn't exist.
+        """
+        return self.get(key, fn=int)
